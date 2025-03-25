cb5132ebfa757c2aeecc74798fd6421307194854
Saad <saadelasfur@gmail.com>
2025-03-25 23:28:44 +0200

    platform: patches: disable encryption and set data to F2FS
    Signed-off-by: ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>

--- platform/exynos990/patches/miscs/customize.sh
+++ platform/exynos990/patches/miscs/customize.sh
@@ -7,3 +7,11 @@ echo "Setting casefold props"
 SET_PROP "external_storage.projid.enabled" "1" "$WORK_DIR/vendor/build.prop"
 SET_PROP "external_storage.casefold.enabled" "1" "$WORK_DIR/vendor/build.prop"
 SET_PROP "external_storage.sdcardfs.enabled" "0" "$WORK_DIR/vendor/build.prop"
+
+echo "Disabling encryption"
+# Encryption
+LINE=$(sed -n "/^\/dev\/block\/by-name\/userdata/=" "$WORK_DIR/vendor/etc/fstab.exynos990")
+sed -i "${LINE}s/,fileencryption=ice//g" "$WORK_DIR/vendor/etc/fstab.exynos990"
+
+# ODE
+sed -i -e "/ODE/d" -e "/keydata/d" -e "/keyrefuge/d" "$WORK_DIR/vendor/etc/fstab.exynos990"
